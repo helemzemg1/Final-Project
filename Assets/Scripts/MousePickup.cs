3ad871f6eb7349ce0e1ea7e456d6d13325ed31bf
helemzemg1 <122691333+helemzemg1@users.noreply.github.com>
2023-04-15 09:01:57 -0700

    Final Project

---
+++ Assets/Scripts/MousePickup.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MousePickup : MonoBehaviour
+{
+    public float range = 20;
+    public float throwPower = 10;
+    public Transform handTransform;
+
+    public Transform rayOrigin;
+    private GameObject pickedUpItem = null;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (pickedUpItem == null)
+            {
+                RaycastHit hit;
+                if (Physics.Raycast(rayOrigin.position, rayOrigin.transform.forward, out hit, range))
+                {
+                    if (hit.collider.GetComponent<IPickup>() != null)
+                    {
+                        pickedUpItem = hit.collider.gameObject;
+                        pickedUpItem.GetComponent<IPickup>().Pickup(handTransform);
+                    }
+                    
+                }
+            }
+            else
+            {
+                pickedUpItem.GetComponent<IPickup>().Drop(throwPower);
+                pickedUpItem = null;
+            }
+        }
+    }
+}
